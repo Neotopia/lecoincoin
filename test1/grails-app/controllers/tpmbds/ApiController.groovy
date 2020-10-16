5356698fe8b71c1fe2dd8aa87d71af9ba63d489e
Lisa <l.momas@net.estia.fr>
2020-10-16 16:30:56 +0200

    Mise en forme + Patch/delete ressources

--- test1/grails-app/controllers/tpmbds/ApiController.groovy
+++ test1/grails-app/controllers/tpmbds/ApiController.groovy
@@ -6,23 +6,25 @@ import groovy.json.JsonBuilder
 
 class ApiController {
 
-    def index() {
-        println request.JSON.title
-        println request.JSON.price
-        return response.status = 201
-    }
+    // Gestion API REST DU SITE LECOINCOIN
+
 /*
 méthodes GET / PUT  / DELETE
  */
 
+/******* RESSOURCE INDIVIDUEL USER *******/
     def user() {
         switch (request.getMethod()) {
             case "GET":
+                // Vérification de la requête sur l'URL
                 if (!params.id)
                     return response.status = 400
+                // stockage des nouveaux paramètre de la requête dans userInstance
                 def userInstance = User.get(params.id)
+                //Vérification de la correspondance de l'ID de la requête avec la base de donnée
                 if (!userInstance)
                     return response.status = 404
+                //gstion de l'affichage des données sur un Json
                 def builder = new JsonBuilder()
                 builder.user{
                     id userInstance.id
@@ -37,40 +39,38 @@ méthodes GET / PUT  / DELETE
                 }
                 render builder.toPrettyString()
                 break
+
             case "PATCH":
-                if (!params.id)
+                if (!params.id || !request.JSON.username || !request.JSON.password)
                     return response.status = 400
 
                 def userInstance = User.get(params.id) //si l'id est attaché à une instance
                 if (!userInstance)
                     return response.status = 404
-
-                def map = [username: request.JSON.username, params: request.JSON.password]
+                def map = [username: request.JSON.username, password: request.JSON.password]
                 userInstance.properties = map
                 userInstance.save (flush: true)
                 return response.status = 204
                 break
+
             case "DELETE":
-                if(!params.id)
+                if (!params.id)
                     return response.status = 400
-                def userInstance = User.get(params.id) //si l'id est attaché à une instance
+                def userInstance = User.get(params.id)
                 if (!userInstance)
                     return response.status = 404
-                userInstance.delete()
-               // userInstance.save (flush: true)
+                UserRole.removeAll(userInstance)
+                userInstance.delete(flush: true)
                 return response.status = 204
                 break
             default:
                 return response.status = 405
                 break
-        /**************************/
-        // MARCHE PAS
-        /*************************/
         }
         return response.status = 406
     }
 
-
+/******* RESSOURCE INDIVIDUEL SALEAD *******/
     def saleAd() {
 
          switch (request.getMethod()) {
@@ -85,15 +85,17 @@ méthodes GET / PUT  / DELETE
                      json { render saleAdInstance as JSON }
                  }
                 break
-            case "PUT":
-                if (!params.id || !params.title || !params.price || !params.description || !params.longDescription ||
-                        !params.author || !params.illustrations)
+
+            case "PATCH":
+                if (!params.id || !request.JSON.title || !request.JSON.price || !request.JSON.description
+                        || !request.JSON.longDescription)
                     return response.status = 400
                 def saleAdInstance = SaleAd.get(params.id) //si l'id est attaché à une instance
                 if (!saleAdInstance)
                     return response.status = 404
-                def map = [title: params.title, price: params.price, description: params.description,
-                           longDescription: params.longDescription, author: params.author, illustrations: params.illustrations]
+
+                def map = [title: request.JSON.title, price: request.JSON.price, description: request.JSON.description,
+                           longDescription: request.JSON.longDescription]
                 // we then bind data to the model object this way
                 saleAdInstance.properties = map
                 // and save the updated user
@@ -101,39 +103,32 @@ méthodes GET / PUT  / DELETE
                 return response.status = 204
                 break
 
+             case "DELETE":
+                 if(!params.id) {
+                     return response.status = 400
+                 }
+                 def saleAdInstance = SaleAd.get(params.id) //si l'id est attaché à une instance
+                 if (!saleAdInstance) {
+                     return response.status = 404
+                 }
 
-         /**************************/
-         // MARCHE PAS
-         /*************************/
-
-            case "DELETE":
-                f(!params.id)
-                return response.status = 400
-                def userInstance = saleAdInstance.author
-                userInstance.removeFromSaleAds(saleAdInstance)
-                userInstance.save(flush: true)
-
-                def saleAdInstance = SaleAd.get(params.id) //si l'id est attaché à une instance
-                if (!saleAdInstance)
-                    return response.status = 404
-                saleAdInstance.delete()
-                return response.status = 204
-                break
-            default:
-                return response.status = 405
-                break
-
-
+                 saleAdInstance.delete(flush: true)
+                 return response.status = 204
+                 break
+             default:
+                 return response.status = 405
+                 break
 
         }
         return response.status = 406
     }
 
+
     /*
-    méthode GET / POST
+    méthode GET et POST
      */
 
-
+/******* COLLECTION USERS *******/
     def users(){
         switch (request.getMethod()) {
             case "GET":
@@ -149,6 +144,7 @@ méthodes GET / PUT  / DELETE
                 }
                 render builder.toPrettyString()
                 break
+
             case "POST":
                 def userInstance = new User(username: params.username, password: params.password).save(flush: true)
                 UserRole.create(userInstance, Role.get(params.role), flush: true)
@@ -160,7 +156,7 @@ méthodes GET / PUT  / DELETE
         }
         return response.status = 406
     }
-
+/******* COLLECTION SALEADS *******/
     def saleAds(){
         switch (request.getMethod()) {
             case "GET":
@@ -170,6 +166,7 @@ méthodes GET / PUT  / DELETE
                     json { render ListSaleAd as JSON }
                 }
                 break
+            
             case "POST":
                 def saleAdInstance = new SaleAd(
                         title: params.title,
@@ -189,81 +186,3 @@ méthodes GET / PUT  / DELETE
     }
 
 }
-
-/*
-
-page 135
-
-def userList = User.list()
-def builder = new JsonBuilder()
-// Build JSON
-def result = builder.users{
-    // Iterate over each user of the list
-    userList.each{
-        // Create new node for each
-        User userInstance -> user {
-            // Declare all properties
-            id userInstance.id
-            username userInstance.username
-        }
-    }
-}
-// Render content as String
-render builder.toPrettyString()
-
-*/
-
-
-
-
-/*
-ZONE DE TEST
-
-GET USER :
-
-http://localhost:8081/tpmbds/api/user/2
-
-PATCH USER :
-
-http://localhost:8081/tpmbds/api/user/2
-
-{
-    "username":"Nico",
-    "password":"password"
-}
-
-http://localhost:8081/tpmbds/api/user?id=2&username=Nico&password=password
-
-DELETE USER :
-
-
-
-GET USERS :
-
-
-
-POST USER :
-
-
-
-GET SALEAD :
-
-http://localhost:8081/tpmbds/api/saleAd/2
-
-PATCH SALEAD :
-
-
-
-DELETE SALEAD :
-
-
-
-GET SALEADS :
-
-
-
-POST SALEAD :
-
-
- */
-
